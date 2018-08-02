7bca0ae1eaf61e000ceaed2a4cbcdf0185c0b454
Joel Tio <joeltiojinhon@gmail.com>
2018-08-02 21:51:43 +0800

    Add aleph and data.json

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/joeltio/disclojure"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [aleph "0.4.6"]]
   :main ^:skip-aot disclojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
