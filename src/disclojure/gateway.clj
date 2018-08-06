38c3b23c47896506d347e7c08268eb17ad6aecae
Joel Tio <joeltiojinhon@gmail.com>
2018-08-06 21:05:36 +0800

    Add heartbeat function

--- src/disclojure/gateway.clj
+++ src/disclojure/gateway.clj
@@ -1,9 +1,12 @@
 (ns disclojure.gateway
-  (:require [disclojure.http :as http]))
+  (:require [disclojure.http :as http]
+            [manifold.stream :as s]))
 
 ;;; Constants
 (def url-suffix "/gateway")
 (def url-bot-suffix "/gateway/bot")
+
+(def heartbeat-opcode 1)
 
 ;;; Getting the gateway endpoint
 (defn- create-bot-header
@@ -16,3 +19,8 @@
   ([base-url bot-token]
    (http/get-json (str base-url url-bot-suffix)
                   (create-bot-header bot-token))))
+
+
+(defn heartbeat
+  [json-conn seq]
+  @(s/put! {"op" heartbeat-opcode "d" seq}))
