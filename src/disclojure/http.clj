b541a1c54cbc99b588be5ed8fb5741388755c634
Joel Tio <joeltiojinhon@gmail.com>
2018-08-06 20:42:58 +0800

    Add websocket-json-client for duplex json communication

--- src/disclojure/http.clj
+++ src/disclojure/http.clj
@@ -2,6 +2,7 @@
   (:require [aleph.http :as http]
             [byte-streams :as bs]
             [manifold.deferred :as d]
+            [manifold.stream :as s]
             [clojure.data.json :as json]))
 
 (defn- http-body->str
@@ -15,3 +16,21 @@
   (d/chain (apply http/get args)
            http-body->str
            json/read-str))
+
+(defn- connect-via-fn
+  [src f dst]
+  (s/connect-via src #(s/put! dst (f %)) dst))
+
+(defn- stream->json-stream
+  [stream]
+  (let [json-incoming-stream (s/stream)
+        json-outbound-stream (s/stream)]
+    ;; Connect the json streams to the stream
+    (connect-via-fn stream json/read-str json-incoming-stream)
+    (connect-via-fn json-outbound-stream json/write-str stream)
+    ;; Splice the inboud and outbound stream
+    (s/splice json-outbound-stream json-incoming-stream)))
+
+(defn websocket-json-client
+  [url]
+  (d/chain' (http/websocket-client url) stream->json-stream))
