36c500bfdd812fc08eb09cf20535b820c1d421bf
Joel Tio <joeltiojinhon@gmail.com>
2018-08-09 12:14:22 +0800

    Add gateway endpoint params and gateway caching

--- src/disclojure/gateway.clj
+++ src/disclojure/gateway.clj
@@ -3,6 +3,10 @@
             [manifold.stream :as s]))
 
 ;;; Constants
+(def gateway-version 6)
+(def gateway-encoding "json")
+(def gateway-compress "zlib-stream")
+
 (def base-url "https://discordapp.com/api")
 (def url-suffix "/gateway")
 (def url-bot-suffix "/gateway/bot")
@@ -14,12 +18,33 @@
   [bot-token]
   {:headers {"Authorization" (str "Bot " bot-token)}})
 
-(defn get-endpoint
+(defn create-endpoint-params
   ([]
-   (http/get-json (str base-url url-suffix)))
+   (create-endpoint-params gateway-version gateway-encoding gateway-compress))
+  ([version]
+   (create-endpoint-params version gateway-encoding gateway-compress))
+  ([version encoding]
+   (create-endpoint-params version encoding gateway-compress))
+  ([version encoding compress]
+   {:query-params {:v version :encoding encoding :compress compress}}))
+
+(defn- get-endpoint
+  ([]
+   (http/get-json (str base-url url-suffix)
+                  (create-endpoint-params)))
+  ([endpoint-params]
+   (http/get-json (str base-url url-suffix)
+                  endpoint-params)))
+
+(def get-endpoint-memo (memoize get-endpoint))
+
+(defn get-bot-endpoint
   ([bot-token]
+   (get-bot-endpoint bot-token (create-endpoint-params)))
+  ([bot-token endpoint-params]
    (http/get-json (str base-url url-bot-suffix)
-                  (create-bot-header bot-token))))
+                  (create-bot-header bot-token)
+                  endpoint-params)))
 
 (defn create-payload
   ([opcode data]
