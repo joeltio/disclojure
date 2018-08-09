bc273a1befbb5550b6d8eb4788b0826f62e4513e
Joel Tio <joeltiojinhon@gmail.com>
2018-08-09 16:05:32 +0800

    Update create-identify to use map of options

--- src/disclojure/gateway/identify.clj
+++ src/disclojure/gateway/identify.clj
@@ -10,23 +10,31 @@
 (def online-presence
   (presence/create-presence nil nil presence/user-status-online false))
 
+(defn- all-or-none
+  [m ks]
+  (or (not-any? m ks)
+      (every? m ks)))
+
 (defn create-identify
-  [token shard-num total-shards presence]
-  {:token token
-   :properties {:$os *os-name*
-                :$browser library-name
-                :$device library-name}
-   :compress false
-   :large_threshold 250
-   :shard [shard-num total-shards]
-   :presence presence})
+  "Creates the identify payload
+   Options taken are: `:shard-num`, `:total-shards` and `:initial-presence`.
+   `:shard-num` and `:total-shards` must coexist."
+  ([token options]
+   {:pre [(all-or-none options [:shard-num :total-shards])]}
+   (let [presence (or (options :initial-presence) online-presence)]
+     (if (every? options [:shard-num :total-shards])
+       (create-identify token (options :shard-num) (options :total-shards) presence)
+       (create-identify token 0 1 presence))))
+  ([token shard-num total-shards presence]
+   {:token token
+    :properties {:$os *os-name*
+                 :$browser library-name
+                 :$device library-name}
+    :compress false
+    :large_threshold 250
+    :shard [shard-num total-shards]
+    :presence presence}))
 
 (defn identify
-  ([json-conn token]
-   (identify json-conn token 0 1))
-  ([json-conn token shard-num total-shards]
-   (identify json-conn token shard-num total-shards online-presence))
-  ([json-conn token shard-num total-shards presence]
-   (let [identify-payload (create-identify token shard-num
-                                           total-shards presence)]
-     (gateway/send-payload json-conn identify-opcode identify-payload))))
+  [json-conn identify-payload]
+  (gateway/send-payload json-conn identify-opcode identify-payload))
