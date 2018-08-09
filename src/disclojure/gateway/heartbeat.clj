d20e27d034f9d23da1755f4655589296b3a6c977
Joel Tio <joeltiojinhon@gmail.com>
2018-08-09 16:05:50 +0800

    Fix bugs in heartbeat and start-heartbeat now returns the seq atom

--- src/disclojure/gateway/heartbeat.clj
+++ src/disclojure/gateway/heartbeat.clj
@@ -17,7 +17,7 @@
 (defn- heartbeat-responder
   [heartbeat-atom]
   (comp (filter is-heartbeat?)
-        (map (create-heartbeat @(heartbeat-atom)))))
+        (map (create-heartbeat @heartbeat-atom))))
 
 (defn- heartbeat
   [json-conn seq]
@@ -33,17 +33,22 @@
   [json-conn interval]
   (let [heartbeat-seq (atom nil)]
     (t/every interval #(heartbeat json-conn @heartbeat-seq))
-    atom))
+    heartbeat-seq))
 
 (defn add-heartbeat-responder
   [json-conn heartbeat-atom]
-  (let [responder (s/stream 0 heartbeat-responder)]
+  (let [responder (s/stream 0 (heartbeat-responder heartbeat-atom))]
     (s/connect json-conn responder)
     (s/connect responder json-conn)))
+
+(defn- take-interval
+  [json-conn]
+  (get-in @(s/take! json-conn) ["d" "heartbeat_interval"]))
 
 (defn start-heartbeat
   "Shorthand function to start heartbeat functions after taking the interval"
   [json-conn]
-  (->> (@(s/take json-conn) "heartbeat_interval")
-       (start-heartbeat json-conn)
-       (add-heartbeat-responder json-conn)))
+  (let [interval (take-interval json-conn)
+        heartbeat-atom (start-periodic-heartbeat json-conn interval)]
+    (add-heartbeat-responder json-conn heartbeat-atom)
+    heartbeat-atom))
