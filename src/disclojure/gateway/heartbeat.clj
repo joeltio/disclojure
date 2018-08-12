a3e02c2ddc801fcc1f91f9d5378f5a57f72c5c46
Joel Tio <joeltiojinhon@gmail.com>
2018-08-12 11:41:49 +0800

    Rewrite heartbeat, add take-interval and test

--- src/disclojure/gateway/heartbeat.clj
+++ src/disclojure/gateway/heartbeat.clj
@@ -6,71 +6,19 @@
 
 ;;; Constants
 (def heartbeat-opcode 1)
+(def heartbeat-ack-opcode 11)
 (def heartbeat-timeout 1000)
 
-(def ^:private create-heartbeat
-  (partial gateway/create-payload heartbeat-opcode))
-
-(defn is-heartbeat?
-  [payload]
-  (= (payload "op") heartbeat-opcode))
-
-(defn- heartbeat-responder
-  [heartbeat-atom]
-  (comp (filter is-heartbeat?)
-        (map (create-heartbeat @heartbeat-atom))))
-
-(defn- heartbeat
-  [json-conn seq]
-  ;; Send heartbeat
-  (gateway/send-payload json-conn heartbeat-opcode seq)
-  ;; Receive heartbeat
-  (when-not (s/try-take! json-conn false heartbeat-timeout false)
-    ;; Failed, close connection and throw exception
-    (s/close! json-conn)
-    (throw (Exception. "Heartbeat ack timed out or stream take! error"))))
-
-(defn start-periodic-heartbeat
-  [json-conn interval]
-  (let [heartbeat-seq (atom nil)]
-    (t/every interval #(heartbeat json-conn @heartbeat-seq))
-    heartbeat-seq))
-
-(defn add-heartbeat-responder
-  [json-conn heartbeat-atom]
-  (let [responder (s/stream 0 (heartbeat-responder heartbeat-atom))]
-    (s/connect json-conn responder)
-    (s/connect responder json-conn)))
-
-(defn update-heartbeat-atom!
-  [heartbeat-atom val]
-  (swap! heartbeat-atom #(if (nil? %1) %2 (max %1 %2)) val))
-
-(defn fake-connect-via
-  "Fakes a stream connect-via; the callback will always return a deferred true"
-  ([src f dst]
-   (fake-connect-via src f dst nil))
-  ([src f dst options]
-   (s/connect-via src #(do (f %) (d/success-deferred true)) dst options)))
-
-(defn add-heartbeat-incrementer
-  [json-conn heartbeat-atom]
-  (let [batch-filter (s/filter gateway/is-dispatch? json-conn)
-        incrementer (s/stream)]
-    ;; Do a fake connect to incrementer to act as a callback
-    (fake-connect-via batch-filter
-                      #(update-heartbeat-atom! heartbeat-atom (% "s"))
-                      incrementer)))
-
 (defn- take-interval
-  [json-conn]
-  (get-in @(s/take! json-conn) ["d" "heartbeat_interval"]))
+  [conn]
+  (d/chain' (s/take! conn) #(get-in % ["d" "heartbeat_interval"])))
 
-(defn start-heartbeat
-  "Shorthand function to start heartbeat functions after taking the interval"
-  [json-conn]
-  (let [interval (take-interval json-conn)
-        heartbeat-atom (start-periodic-heartbeat json-conn interval)]
-    (add-heartbeat-responder json-conn heartbeat-atom)
-    (add-heartbeat-incrementer json-conn heartbeat-atom)
-    heartbeat-atom))
+#_(defn start-heartbeat
+  [conn event-bus heartbeat-atom]
+  ;; Add periodic heartbeater
+  (start-periodic-heartbeat conn event-bus
+                            heartbeat-atom @(take-interval conn))
+  ;; Add heartbeat responder
+  (add-heartbeat-responder conn event-bus heartbeat-atom)
+  ;; Add heartbeat incrementer
+  (add-heartbeat-incrementer event-bus heartbeat-atom))
--- test/disclojure/gateway/heartbeat_test.clj
+++ test/disclojure/gateway/heartbeat_test.clj
@@ -1,53 +1,13 @@
 (ns disclojure.gateway.heartbeat-test
   (:require [clojure.test :refer :all]
             [disclojure.gateway.heartbeat :as heartbeat]
+            [manifold.deferred :as d]
             [manifold.stream :as s]))
 
-(defn heartbeat-payload-test
-  [pred normal? ack? other?]
-  (testing "heartbeat payload"
-    (let [payload {"op" heartbeat/heartbeat-opcode}]
-      (is (= (pred payload) normal?))))
-  (testing "heartbeat ack payload"
-    (let [payload {"op" heartbeat/heartbeat-ack-opcode}]
-      (is (= (pred payload) ack?))))
-  (testing "other payload"
-    (let [payload {"op" 0}]
-      (is (= (pred payload) other?)))))
-
-(deftest is-heartbeat?-test
-  (heartbeat-payload-test heartbeat/is-heartbeat? true false false))
-
-(deftest is-heartbeat-ack?-test
-  (heartbeat-payload-test heartbeat/is-heartbeat-ack? false true false))
-
-(deftest is-heartbeat-related?-test
-  (heartbeat-payload-test heartbeat/is-heartbeat-related? true true false))
-
-(deftest periodic-heartbeat-test
-  (testing "heartbeat every interval"
-    (let [rx (s/stream)
-          tx (s/stream)
-          conn (s/splice tx rx)]
-      ;; Start periodic heartbeat and wait for interval
-      (heartbeat/start-periodic-heartbeat conn 1000)
-      ;; Try to take the heartbeat
-      (is @(s/try-take! tx false 100 false))
-      (s/put! rx {"op" 11})
-      (Thread/sleep 1000)
-      (is @(s/try-take! tx false 100 false))
-      (s/close! conn))))
-
-(deftest heartbeat-responder-test
-  (testing "responder responds to heartbeat requests"
-    (let [rx (s/stream)
-          tx (s/stream)
-          conn (s/splice tx rx)
-          heartbeat-atom (atom nil)
-          payload {"op" 1 "s" @heartbeat-atom}]
-      ;; Connect responder to the connection
-      (heartbeat/connect-responder conn heartbeat-atom)
-      ;; Request for heartbeat
-      (s/put! rx payload)
-      (is (= @(s/try-take! tx false 100 false)
-             payload)))))
+(deftest take-interval-test
+  (let [conn (s/stream)
+        interval 1000]
+    (s/put! conn {"d" {"heartbeat_interval" interval}})
+    (let [interval-deferred (#'heartbeat/take-interval conn)]
+      (is (d/deferred? interval-deferred))
+      (is (= @interval-deferred interval)))))
