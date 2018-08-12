4b272b1c1aef4134230bbe5b296f7869663c70f0
Joel Tio <joeltiojinhon@gmail.com>
2018-08-12 15:39:49 +0800

    Add periodic heartbeat function and test

--- src/disclojure/gateway/heartbeat.clj
+++ src/disclojure/gateway/heartbeat.clj
@@ -2,16 +2,52 @@
   (:require [disclojure.gateway :as gateway]
             [manifold.time :as t]
             [manifold.stream :as s]
-            [manifold.deferred :as d]))
+            [manifold.deferred :as d]
+            [manifold.bus :as b]
+            [manifold.executor :as ex])
+  (:import [java.util.concurrent ScheduledThreadPoolExecutor]))
 
 ;;; Constants
 (def heartbeat-opcode 1)
 (def heartbeat-ack-opcode 11)
 (def heartbeat-timeout 1000)
 
+(def ^:private thread-name-prefix "heartbeat-pool-")
+
 (defn- take-interval
   [conn]
   (d/chain' (s/take! conn) #(get-in % ["d" "heartbeat_interval"])))
+
+(defn- create-heartbeat-clock
+  [thread-pool-size]
+  (let [cnt (atom 0)
+        name-generator #(str thread-name-prefix (swap! cnt inc))]
+    (->> (ex/thread-factory name-generator (deliver (promise) nil))
+         (ScheduledThreadPoolExecutor. thread-pool-size)
+         t/scheduled-executor->clock)))
+
+(defn- heartbeat
+  [conn ack-subscriber heartbeat-atom]
+  ;; Send the heartbeat
+  (gateway/send-payload conn heartbeat-opcode @heartbeat-atom)
+  ;; Try to receive a heartbeat ack
+  (when-not @(s/try-take! ack-subscriber false heartbeat-timeout false)
+    ;; Failed, close connection and throw exception
+    (s/close! conn)
+    (throw (Exception. "Heartbeat ack timed out or stream take! error"))))
+
+(defn- start-periodic-heartbeat
+  [conn event-bus heartbeat-atom interval]
+  (let [heartbeat-ack-subscriber (b/subscribe event-bus heartbeat-ack-opcode)
+        clock (create-heartbeat-clock 1)]
+    ;; A clock is required here as t/every and t/try-take! both use the same
+    ;; clock (manifold's *clock*), and the clock is single-threaded. Without an
+    ;; additional clock, it will deadlock.
+    ;; Oddly enough, t/with-clock seems to only bind to t/every, so only one
+    ;; thread is needed for the clock.
+    (t/with-clock clock
+      (t/every interval
+               #(heartbeat conn heartbeat-ack-subscriber heartbeat-atom)))))
 
 #_(defn start-heartbeat
   [conn event-bus heartbeat-atom]
--- test/disclojure/gateway/heartbeat_test.clj
+++ test/disclojure/gateway/heartbeat_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [disclojure.gateway.heartbeat :as heartbeat]
             [manifold.deferred :as d]
-            [manifold.stream :as s]))
+            [manifold.stream :as s]
+            [manifold.bus :as b]))
 
 (deftest take-interval-test
   (let [conn (s/stream)
@@ -11,3 +12,28 @@
     (let [interval-deferred (#'heartbeat/take-interval conn)]
       (is (d/deferred? interval-deferred))
       (is (= @interval-deferred interval)))))
+
+(deftest periodic-heartbeat-test
+  (let [rx (s/stream)
+        tx (s/stream)
+        conn (s/splice tx rx)
+        event-bus (b/event-bus)
+        heartbeat-atom (atom nil)
+        interval 1000
+        response-leeway 100]
+    ;; Connect conn to event-bus
+    (s/connect-via conn #(b/publish! event-bus (% "op") %) (s/stream))
+    ;; Start periodic heartbeat and wait for interval
+    (#'heartbeat/start-periodic-heartbeat conn event-bus heartbeat-atom interval)
+    (testing "heartbeat every of the given interval"
+      ;; Take heartbeat and send back an ack
+      (is @(s/try-take! tx false response-leeway false))
+      (s/put! rx {"op" 11})
+      (Thread/sleep 1000)
+      ;; Take heartbeat and send back an ack
+      (is @(s/try-take! tx false response-leeway false))
+      (s/put! rx {"op" 11})
+      (Thread/sleep 1000))
+    (testing "connection closes when no ack is received"
+      (Thread/sleep (+ heartbeat/heartbeat-timeout response-leeway))
+      (is (s/closed? conn)))))
